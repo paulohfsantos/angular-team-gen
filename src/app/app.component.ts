2e43df42dd4b2c95b3eb75087224530aba50d94d
Paulo Santos <paulohfsantos1@gmail.com>
2022-05-23 02:19:12 -0300

    - set project team generator - name form - number teams input - logic - styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,66 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'team-gen-app';
+
+  newMember = "";
+  members: string[] = [];
+  errorMsg = "";
+  numberOfTeams: number | string = "";
+  teams: string[][] = [];
+
+  onInput(member: string) {
+    this.newMember = member;
+  }
+
+  onNumTeamsInput(n: string) {
+    this.numberOfTeams = Number(n);
+  }
+
+  addMember() {
+    if (!this.newMember) {
+      this.errorMsg = "Please enter a name";
+      return
+    }
+
+    this.errorMsg = "";
+    this.members.push(this.newMember);
+    this.newMember = "";
+  }
+
+  setNumberOfTeams() {
+    if (!this.numberOfTeams || this.numberOfTeams <= 0) {
+      this.errorMsg = "Please enter a number";
+      return
+    }
+
+    if(this.members.length < this.numberOfTeams) {
+      this.errorMsg = "Please enter more members";
+      return
+    }
+
+    this.errorMsg = "";
+
+    const allMembers = [...this.members];
+
+    while (allMembers.length) {
+      for (let i = 0; i < this.numberOfTeams; i++) {
+        const random = Math.floor(Math.random() * allMembers.length);
+        const member = allMembers.splice(random, 1)[0];
+
+        if(!member) {
+          break
+        }
+
+        if (this.teams[i]) {
+          this.teams[i].push(member);
+        } else {
+          this.teams[i] = [member];
+        }
+      }
+    }
+
+    this.members = [];
+    this.numberOfTeams = "";
+    this.errorMsg = "";
+  }
 }
